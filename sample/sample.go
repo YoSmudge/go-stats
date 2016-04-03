b16683c4ce9124f37b31e2af222a6f7e5eef65ff
Sam Rudge <sam.rudge@songkick.com>
2016-04-03 19:54:07 +0100

    Gofmt sample

--- sample/sample.go
+++ sample/sample.go
@@ -1,36 +1,36 @@
 package main
 
-import(
-  "time"
-  "fmt"
+import (
 	"crypto/rand"
-  "runtime"
-  "github.com/samarudge/go-stats"
+	"fmt"
+	"github.com/samarudge/go-stats"
+	"runtime"
+	"time"
 )
 
 func main() {
-  gostats.Start("localhost:8127", 1, "sample")
-  time.Sleep(5*time.Second)
+	gostats.Start("localhost:8127", 1, "sample")
+	time.Sleep(5 * time.Second)
 
-  var randBytes []byte
-  bytesToRead := 128*1024
-  for i := 0;  i<=bytesToRead; i++ {
-    byteChunk := make([]byte, 1024)
-    _, err := rand.Read(byteChunk)
-    if err != nil {
-      fmt.Println("error:", err)
-      return
-    }
+	var randBytes []byte
+	bytesToRead := 128 * 1024
+	for i := 0; i <= bytesToRead; i++ {
+		byteChunk := make([]byte, 1024)
+		_, err := rand.Read(byteChunk)
+		if err != nil {
+			fmt.Println("error:", err)
+			return
+		}
 
-    randBytes = append(randBytes, byteChunk...)
+		randBytes = append(randBytes, byteChunk...)
 
-    time.Sleep((60*time.Second)/time.Duration(bytesToRead))
-  }
+		time.Sleep((60 * time.Second) / time.Duration(bytesToRead))
+	}
 
-  time.Sleep(30)
+	time.Sleep(30)
 
-  randBytes = []byte{}
-  runtime.GC()
+	randBytes = []byte{}
+	runtime.GC()
 
-  time.Sleep(10)
+	time.Sleep(10)
 }
