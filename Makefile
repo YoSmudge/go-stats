fa85afa3f65c931c0f329f783d5e0b018429c212
Sam Rudge <sam.rudge@songkick.com>
2016-04-13 12:22:38 +0100

    Use makefile for tests and open up pprof endpoint in sample

---
+++ Makefile
@@ -0,0 +1,7 @@
+
+GOFILES=$(shell find . -path ./vendor -prune -o -name '*.go' -print)
+
+test:
+	go build
+	go test -v ./
+	gofmt -l ${GOFILES} | read && echo "gofmt failures" && gofmt -d ${GOFILES} && exit 1
--- sample/sample.go
+++ sample/sample.go
@@ -4,14 +4,20 @@ import (
 	"crypto/rand"
 	"fmt"
 	"github.com/samarudge/go-stats"
+	"net/http"
+	_ "net/http/pprof"
 	"runtime"
 	"time"
 )
 
 func main() {
+	go http.ListenAndServe("localhost:6060", nil)
+
 	gostats.Start("localhost:8127", 1, "sample")
+
 	time.Sleep(5 * time.Second)
 
+	fmt.Println("Starting generation of random bytes")
 	var randBytes []byte
 	bytesToRead := 128 * 1024
 	for i := 0; i <= bytesToRead; i++ {
@@ -26,11 +32,13 @@ func main() {
 
 		time.Sleep((60 * time.Second) / time.Duration(bytesToRead))
 	}
-
+	fmt.Println(" - Done")
 	time.Sleep(30)
 
+	fmt.Println("Forcing GC")
 	randBytes = []byte{}
 	runtime.GC()
+	fmt.Println("Done")
 
 	time.Sleep(10)
 }
